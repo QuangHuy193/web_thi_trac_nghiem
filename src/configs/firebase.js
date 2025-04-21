6df57fe3c0fc7544b3a5832d907a3fb0285b8e13
QuangHuy193 <quanghuylhpq@gmail.com>
2025-04-21 20:00:54 +0700

    fix lỗi đăng nhập google trên host #1

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: "web-thi-trac-nghiem.firebasestorage.app",
   messagingSenderId: "25598118311",
   appId: "1:25598118311:web:22b04d865d91e0bfe70e82",
+  measurementId: "G-4YSTC7ZMVE"
 };
 
 const app = initializeApp(firebaseConfig);
