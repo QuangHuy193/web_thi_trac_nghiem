e24260072f75b7bd4a840ece37ff2aca4e7d0ed0
Tô Đan Huy <todanhuy02102003@gmail.com>
2025-04-23 00:14:34 +0700

    update homepage

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -16,7 +16,11 @@ import MakeExam from "../../components/MakeExam/MakeExam";
 import ListExam from "../../components/ListExam/ListExam";
 import HistoryExam from "../../components/HistoryExam/HistoryExam";
 import Loading from "../../components/Loading/Loading";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
+
+import Slider from "react-slick";
 const cx = classNames.bind(styles);
 
 function Home() {
@@ -143,6 +147,60 @@ function Home() {
           )}
 
           {selectedContent === "info" && <Info user={user} setUser={setUser} />}
+          {!selectedContent && (
+            <div className={cx("home")}>
+              <Slider
+                dots={true}
+                infinite={true}
+                speed={500}
+                slidesToShow={1}
+                slidesToScroll={1}
+                autoplay={true}
+                autoplaySpeed={3000}
+              >
+                <div>
+                  <img src="/bannertoan.jpg" alt="Banner 1" className={cx("banner")} />
+                </div>
+                <div>
+                  <img src="/banner2.jpg" alt="Banner 2" className={cx("banner")} />
+                </div>
+                <div>
+                  <img src="/banner3.jpg" alt="Banner 3" className={cx("banner")} />
+                </div>
+                <div>
+                  <img src="/banner4.jpg" alt="Banner 4" className={cx("banner")} />
+                </div>
+              </Slider>
+              <div className={cx("card-container")}>
+                <div className={cx("card")}>
+                  <img src="/sachmoi.jpg" alt="Đề thi học kỳ" />
+                  <h3>ĐỀ THI HỌC KỲ</h3>
+                  <p>
+                    Ngân hàng câu hỏi đầy đủ các môn cấp 1,2,3 được trộn tạo đề theo cấu trúc giúp luyện tập hiệu quả.
+                  </p>
+                </div>
+
+                <div className={cx("card")}>
+                  <img src="/sachnon.jpg" Đề thi THPT QG />
+                  <h3>ĐỀ THI THPT QG</h3>
+                  <p>
+                    Tổng hợp đề thi trắc nghiệm của các môn khối thi THPT QG, có đáp án & giải thích chi tiết.
+                  </p>
+                </div>
+
+                <div className={cx("card")}>
+                  <img src="/khoahoc.jpg" alt="Đề kiểm tra" />
+                  <h3>ĐỀ KIỂM TRA</h3>
+                  <p>
+                    Thi thử 15 phút, 1 tiết theo hình thức online, kết quả được chấm điểm tự động.
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+
+
+
           {selectedContent === "history" && (
             <History
               setSelectedContent={setSelectedContent}
